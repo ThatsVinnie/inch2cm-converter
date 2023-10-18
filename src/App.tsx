1d137de77a68cbe0d95e16b45b6b44be6a3ad0e7
Vinícius Barbosa <vinibar95@gmail.com>
2023-10-18 17:44:16 -0300

    Upload of Inch2Cm-converter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
+import { LanguageVariant } from 'typescript';
 import './App.css';
+import { useState } from 'react';
+import { FaArrowRightArrowLeft } from 'react-icons/fa6'
 
 function App() {
+  const cm:number = 2.54
+  const inches:number = 0.393701
+
+  const [cent, setCent] = useState<number>(0)
+  const [inch, setInch] = useState<number>(0)
+  const [toggle, setToogle] = useState<boolean>(true)
+
+  const handleConvertCm = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    setCent(parseFloat(e.target.value)*cm)
+    return 
+  }
+
+  const handleConvertInch = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    setInch(parseFloat(e.target.value)*inches)
+    return
+  }
+
+  function handleChange(){
+    setToogle(!toggle)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main className='main-container'>
+        
+        <span className='title'>Converter</span>
+        
+        <span className='subtitle-container'>
+          Choose between inches or centimeters
+        </span>
+        {toggle ? (
+          <div className='content-container'>
+            <div className='convert-container'>
+              <span className='span-first'>
+                <span className='span-measure-left'>
+                  <label htmlFor="inches">I n</label>
+                </span>
+                <span className='input-measure-container'>
+                  <input  type="number" 
+                          id="inches"
+                          onChange={(e)=>handleConvertCm(e)}
+                          className='input-measure'/>
+                  Inches
+                </span>
+
+              </span>
+              <button className='switch-button'>
+                <FaArrowRightArrowLeft onClick={handleChange}/>
+              </button>
+              <span className='span-second'>
+                <span className='span-measure-right'>
+                  <label htmlFor="cent">C m</label>
+                </span>
+                <span className='input-measure-container'>
+                  <input  type="number" 
+                          id="cent"
+                          value={cent}
+                          className='input-measure'/>
+                  Centimeters
+                </span>
+
+              </span>
+            </div>
+            <div className='pic-container'>
+
+            </div>
+          </div>
+        ):(
+          <div className='content-container'>
+            <div className='convert-container'>
+              <span className='span-first'>
+                <span className='span-measure-left'>
+                  <label htmlFor="cent">C m</label>
+                </span>
+                <span className='input-measure-container'>
+                  <input  type="number" 
+                          id="cent"
+                          onChange={(e)=>handleConvertInch(e)}
+                          className='input-measure'/>
+                  Centimeters
+                </span>
+              </span>
+              <button className='switch-button'>
+                <FaArrowRightArrowLeft onClick={handleChange}/>
+              </button>
+              
+
+              <span className='span-second'>
+                <span className='span-measure-right'>
+                  <label htmlFor="inches">I n</label> 
+                </span>
+                <span className='input-measure-container'>
+                  <input  type="number" 
+                          id="inches"
+                          value={inch}
+                          className='input-measure'/>
+                  Inches
+                </span>
+              </span>
+            </div>
+            <div className='pic-container'>
+            
+            </div>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
